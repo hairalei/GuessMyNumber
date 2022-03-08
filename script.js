a5a0550d5cf251a47b7d558e09b0bd42c2288098
Haira Lei <darhaii13@gmail.com>
2022-03-08 15:17:06 +0800

    Create variables for DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const guess = document.querySelector('.main__guess');
+const number = document.querySelector('.header__number');
+const btnAgain = document.querySelector('.btn--again');
+const btnCheck = document.querySelector('.btn--check');
+const message = document.querySelector('.main__message');
+const highScore = document.querySelector('.main__highscore');
+const score = document.querySelector('.main__score');
