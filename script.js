fd73ccd03820345a0d18797f287347d5a2892f7a
Haira Lei <darhaii13@gmail.com>
2022-03-08 21:13:46 +0800

    Implement the game logic. Not a copy paste code and used different approach from Jonas.

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
 'use strict';
 
+const body = document.querySelector('body');
 const guess = document.querySelector('.main__guess');
 const number = document.querySelector('.header__number');
 const btnAgain = document.querySelector('.btn--again');
 const btnCheck = document.querySelector('.btn--check');
 const message = document.querySelector('.main__message');
-const highScore = document.querySelector('.main__highscore');
+const highscore = document.querySelector('.main__highscore');
 const score = document.querySelector('.main__score');
+
+let gameover = false;
+let scoreNum = 20;
+let highscoreNum = 0;
+let num;
+
+function randomNumber() {
+  return Math.floor(Math.random() * 20) + 1;
+}
+
+function checkGuess() {
+  let guessNum = parseInt(guess.value);
+  console.log(typeof guessNum);
+
+  if (!guessNum) {
+    message.textContent = 'Invalid number!';
+  } else if (guessNum === num) {
+    number.textContent = num;
+    message.textContent = 'Congatulations! You win!';
+    body.style.backgroundColor = '#60b347';
+    score.textContent = scoreNum;
+    getHighscore();
+    gameover = true;
+    btnCheck.disabled = true;
+  } else if (guessNum !== num) {
+    message.textContent = guessNum < num ? 'Too low!' : 'Too high';
+    checkScore();
+    score.textContent = scoreNum;
+  }
+}
+
+function checkScore() {
+  scoreNum > 0 ? scoreNum-- : (scoreNum = 0);
+
+  if (scoreNum === 0) {
+    message.textContent = 'Game over!!!!';
+    btnCheck.disabled = true;
+  }
+}
+
+function getHighscore() {
+  if (scoreNum >= highscoreNum) highscoreNum = scoreNum;
+  highscore.textContent = highscoreNum;
+}
+
+function newGame() {
+  num = randomNumber();
+  number.textContent = '?';
+  body.style.backgroundColor = '#000';
+  guess.value = '';
+  scoreNum = 20;
+  score.textContent = scoreNum;
+  btnCheck.disabled = false;
+}
+
+btnAgain.addEventListener('click', newGame);
+btnCheck.addEventListener('click', checkGuess);
+
+newGame();
